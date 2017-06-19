14d9c783c1cb950c68d97f544f2e684e3ff7f6af
peterennis <peterennis@yahoo.com>
2017-06-19 12:45:51 -0700

    integrate primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { InputTextModule, ButtonModule } from 'primeng/primeng';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -14,7 +16,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    InputTextModule,
+    ButtonModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +28,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { InputTextModule, ButtonModule } from 'primeng/primeng';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  name: string;
+  message: string;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onClick() {
+    this.message = 'Hello ' + this.name;
   }
 
 }
